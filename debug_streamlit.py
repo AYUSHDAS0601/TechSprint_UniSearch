0aaa838d424497fa0b25d1df7d2ca876badf56a7
AYUSHDAS <dasayush.0601@gmail.com>
2026-03-29 01:57:29 +0530

    rework

--- debug_streamlit.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-st.set_page_config(page_title="Debug App")
-st.title("Debug App")
-st.write("If you can see this, Streamlit is working.")
--- monitor_daemon.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-from src.monitor import start_monitoring
-
-if __name__ == "__main__":
-    start_monitoring()
